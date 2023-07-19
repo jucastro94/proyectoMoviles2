55041f80b33ca92f5a3d4ba4264bf4d14f2eccbb
Juan Miguel Castro Rojas <107083834+jucastro94@users.noreply.github.com>
2023-07-18 20:12:45 -0500

    Update common.ts
    update

--- src/app/utilities/common.ts
+++ src/app/utilities/common.ts
@@ -44,7 +44,8 @@ export const onInputChange = (event: Event): string => {
 /**
  * direccion de la api
  */
-export const API = 'http://localhost:3000';
+//export const API = 'http://localhost:3000';
+export const API = 'https://apimovilesdosfinal.azurewebsites.net';
 
 /**
  * convetir un string dado por un usuario en uno valido para usar en db o js objects. haciendo lo siguiente:
